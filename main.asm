3b00e3803ac8fa611fd6dde461cb084823f2d02d
Agustin Lopez Gasio <105187047+Pinchecomadreja@users.noreply.github.com>
2025-06-17 08:12:51 -0300

    update main
    Mods de Caro y Enzo
    - Armado general de la rutina de juego
    - Agrega funciones de compar palabra de .txt con input Player2

--- main.asm
+++ main.asm
@@ -1,78 +1,146 @@
 .8086
 .model small
 .stack 100h
+
 .data
-	input 	db 24h,0dh,0ah,24h
-	salida 	db "HAS SALIDO DEL JUEGO",0dh,0ah,24h
+    input       db 24h,0dh,0ah,24h
+    salida      db "HAS SALIDO DEL JUEGO",0dh,0ah,24h
+    tiempo_mostrar dw 182    ; ≈3 segundos (18.2 ticks/seg * 3)
+    tiempo_dibujo dw 182    ; ≈10 segundos para dibujar
+    tiempo_adivinar dw 300   ; ≈5 segundos para adivinar
+    mensaje_tiempo db "TIEMPO RESTANTE: ", 24h
+    input_adivinar db 20 dup(?) ; Buffer para la palabra a adivinar
+    presione_enter db 0dh,0ah,"Presione ENTER para continuar...",24h
+    animal db "ANIMAL: Leon", 0dh, 0ah, 24h
+    instruccion db "Dibuja lo que viste!", 0dh, 0ah, 24h
 .code
-;---DECLARO LIBRERIA EXTERNA
-	extrn carteles:proc
-						;RECIBE EN AL NRO DE CARTEL A MOSTRAR
-						;IMPRIME CARTEL
-	extrn leertxt:proc
-						;RECIBE EN AL NRO DE PALABRA A BUSCAR
-						;LEE ARCHIVO DE DIC.TXT
-						;IMPRIME PALABRA EN INDEX[NRO]
-	extrn men:proc
-						;RECIBE INPUT POR STACK
-						;IMPRIME MENU
-						;INTERACCION INTERNA EN AL
-						;1.CONTINUAR (SI)
-						;2.SALIR (NO)
-	extrn cls:proc
-						;LIMPIA LA PANTALLA
-	extrn print:proc
-						;IMPRIME VARS
-	extrn pincel:proc
-						;IMPRIME ESPECIAL VARS
+    extrn carteles:proc, leertxt:proc, men:proc, cls:proc
+    extrn print:proc, pincel:proc, delayTicks:proc
 
-	main proc
+main proc
+    mov ax, @data
+    mov ds, ax
 
-	mov ax,@data
-	mov ds,ax
+    call cls
 
-	call cls
+    ; Mostrar menú principal
+    mov dx, offset input
+    push dx
+    call men
 
-	mov dx,offset input
-	push dx
-	call men
+    lea bx, input
+    call cls
 
-	lea bx,input
-	call cls
+    opciones:
+        cmp byte ptr[bx], '1'
+        je op1
+        cmp byte ptr[bx], '2'
+        je fin
 
-	opciones:
-		cmp byte ptr[bx],'1'
-		je op1
+    op1:
+        ; Fase de visualización - Jugador 1
+        mov al, '1'
+        call carteles
+        
+        ; Mostrar el animal a dibujar
+        mov dx, offset animal
+        call print
+        
+        ; Esperar 3 segundos
+        mov cx, tiempo_mostrar
+        call DelayTicks
+        
+        ; Limpiar pantalla
+        call cls
+        
+        ; Mostrar instrucción para dibujar
+        mov dx, offset instruccion
+        call print
+        
+        ; Iniciar temporizador y herramienta de dibujo
+        mov cx, tiempo_dibujo
+        call pincel
+        
+        ; Limpiar pantalla
+        call cls
+        
+        ; Fase de adivinar - Jugador 2
+        mov al, '2'
+        call carteles
+        
+        ; Mostrar mensaje de tiempo
+        mov dx, offset mensaje_tiempo
+        call print
+        
+        ; Esperar entrada del jugador 2
+        mov cx, tiempo_adivinar
+        call leer_respuesta
+        
+        ; Mensaje para continuar
+        mov dx, offset presione_enter
+        call print
+        mov ah, 1
+        int 21h
+        
+        jmp fin
 
-		cmp byte ptr[bx],'2'
-		je fin
+    fin:
+        call cls
+        mov ah, 9
+        mov dx, offset salida
+        int 21h
+        
+        mov ah, 4ch
+        int 21h
+main endp
 
-	op1:
-	;MUESTRA CARTEL PLAYER1
-		xor ax,ax
+; Función para leer respuesta
+leer_respuesta proc
+    push bx
+    push cx
+    push dx
+    
+    mov bx, offset input_adivinar
+    mov cx, 0  ; Contador de caracteres
+    
+lectura:
+    ; Verificar tiempo
+    mov ah, 1
+    int 16h
+    jz no_tecla
+    
+    ; Leer tecla
+    mov ah, 0
+    int 16h
+    
+    cmp al, 0Dh  ; Enter
+    je fin_lectura
+    
+    ; Guardar caracter
+    mov [bx], al
+    inc bx
+    inc cx
+    
+    ; Mostrar caracter
+    mov dl, al
+    mov ah, 2
+    int 21h
+    
+no_tecla:
+    ; Verificar tiempo restante
+    ; (Aquí iría la lógica para verificar el timer)
+    jmp lectura
+    
+fin_lectura:
+    ; Terminar string
+    mov byte ptr [bx], '$'
+    
+    pop dx
+    pop cx
+    pop bx
+    ret
+leer_respuesta endp
 
-		;IMPRIME CARTEL PLAYER1
-		mov al,'1'
-		call carteles
+; Función DelayTicks corregida
 
-		xor ax,ax
-		;PUT TIMER 5 SEG
-		call pincel
-
-		;IMPRIME CARTEL PLAYER1
-		;mov al,'2'
-		;call carteles
-
-	
-	fin:
-		call cls
-		mov ah,9
-		mov dx, offset salida
-		int 21h
-		
-		mov ah,4ch
-		int 21h
-
-	main endp
-
-end
+end main
